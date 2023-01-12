64434b7e93fae0ea2ea8ea914a6925817870245f
Amanda Dias da Costa <79376043+mandy-87@users.noreply.github.com>
2023-01-11 21:18:18 -0300

    Add files via upload

---
+++ js/api.js
@@ -0,0 +1,8 @@
+let api_key = "40330edce6b302bc1b706bc1dc83fcec"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
+
